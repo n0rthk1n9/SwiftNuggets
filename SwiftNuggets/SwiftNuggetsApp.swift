ae55a659091abf61614ae8db7dba3c5ad4139c84
Jan Armbrust <jan.armbrust@googlemail.com>
2024-11-21 20:52:15 +0100

    Initial commit

---
+++ SwiftNuggets/SwiftNuggetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNuggetsApp.swift
+//  SwiftNuggets
+//
+//  Created by Jan Armbrust on 21.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftNuggetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
