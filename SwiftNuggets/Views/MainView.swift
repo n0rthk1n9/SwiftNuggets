7492e5958f3d0946f453554849de03ac04ffb1b4
Jan Armbrust <jan.armbrust@googlemail.com>
2024-11-21 21:01:47 +0100

    Add skeleton

---
+++ SwiftNuggets/Views/MainView.swift
@@ -0,0 +1,29 @@
+//
+//  MainView.swift
+//  SwiftNuggets
+//
+//  Created by Jan Armbrust on 21.11.2024.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var views: [(title: String, view: AnyView)] = [
+        (title: "Swift Nugget 1", view: AnyView(SwiftNugget1()))
+    ]
+
+    var body: some View {
+        NavigationStack {
+            List(views, id: \.title) { item in
+                NavigationLink(destination: item.view) {
+                    Text(item.title)
+                }
+            }
+            .navigationTitle("Swift Nuggets 🍗")
+        }
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ SwiftNuggets/Views/NuggetViews/SwiftNugget1.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftNugget1.swift
+//  SwiftNuggets
+//
+//  Created by Jan Armbrust on 21.11.2024.
+//
+
+import SwiftUI
+
+struct SwiftNugget1: View {
+    var body: some View {
+        Text("SwiftNugget1")
+    }
+}
+
+#Preview {
+    SwiftNugget1()
+}
