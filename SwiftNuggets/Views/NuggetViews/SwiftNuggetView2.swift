ffd0fa291fbdc219432bf4283d3c2a410f5f319a
Jan Armbrust <jan.armbrust@googlemail.com>
2024-11-22 18:45:58 +0100

    Refine Swift Nugget 2

--- SwiftNuggets/Views/NuggetViews/SwiftNuggetView2.swift
+++ SwiftNuggets/Views/NuggetViews/SwiftNuggetView2.swift
@@ -9,7 +9,42 @@ import SwiftUI
 
 struct SwiftNuggetView2: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Form {
+                Section("Text") {
+                    TextField(".keyboardType: emailAddress", text: .constant(""))
+                        .keyboardType(.emailAddress)
+                    TextField(".keyboardType: alphabet", text: .constant(""))
+                        .keyboardType(.alphabet)
+                    TextField(".keyboardType: asciiCapable", text: .constant(""))
+                        .keyboardType(.asciiCapable)
+                }
+                Section("Numbers") {
+                    TextField(".keyboardType: numberPad", text: .constant(""))
+                        .keyboardType(.numberPad)
+                    TextField(".keyboardType: decimalPad", text: .constant(""))
+                        .keyboardType(.decimalPad)
+                    TextField(".keyboardType: numbersAndPunctuation", text: .constant(""))
+                        .keyboardType(.numbersAndPunctuation)
+                    TextField(".keyboardType: namePhonePad", text: .constant(""))
+                        .keyboardType(.namePhonePad)
+                    TextField(".keyboardType: asciiCapableNumberPad", text: .constant(""))
+                        .keyboardType(.asciiCapableNumberPad)
+                    TextField(".keyboardType: phonePad", text: .constant(""))
+                        .keyboardType(.phonePad)
+                }
+                Section("Special") {
+                    TextField(".keyboardType: webSearch", text: .constant(""))
+                        .keyboardType(.webSearch)
+                    TextField(".keyboardType: URL", text: .constant(""))
+                        .keyboardType(.URL)
+                    TextField(".keyboardType: twitter", text: .constant(""))
+                        .keyboardType(.twitter)
+                    
+                }
+            }
+            .navigationTitle("Swift Nugget 2")
+        }
     }
 }
 
