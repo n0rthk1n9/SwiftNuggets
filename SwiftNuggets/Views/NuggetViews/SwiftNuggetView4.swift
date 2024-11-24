52d7b22bef632bd2ab702df84e87a0ca83e92e78
Jan Armbrust <jan.armbrust@googlemail.com>
2024-11-24 20:06:31 +0100

    Refine Swift Nugget 4

--- SwiftNuggets/Views/NuggetViews/SwiftNuggetView4.swift
+++ SwiftNuggets/Views/NuggetViews/SwiftNuggetView4.swift
@@ -9,16 +9,20 @@ import SwiftUI
 
 struct SwiftNuggetView4: View {
     @State private var amount: Double = 0.0
-    private var localCurrencySymbol: String? = Locale.current.currency?.identifier
+    
+    private var localCurrencyCode: String? = Locale.current.currency?.identifier
     
     var body: some View {
         NavigationStack {
             Form {
                 Section("Formatted currency amount") {
-                    TextField("Format currency", value: $amount, format: .currency(code: localCurrencySymbol ?? "EUR"))
+                    TextField(
+                        "Format currency",
+                        value: $amount,
+                        format: .currency(code: localCurrencyCode ?? "EUR")
+                    )
                         .keyboardType(.decimalPad)
                 }
-                
             }
             .navigationTitle("Swift Nugget 4")
         }
